99d138f3f171bbe90b91fcfa05361e900e4ac1a4
Rafael <rvillegasm@eafit.edu.co>
2020-05-24 16:58:00 -0500

    More Changes

--- mpi/src/transcode.cpp
+++ mpi/src/transcode.cpp
@@ -6,9 +6,9 @@
 
 char *transcode(std::string &secuence, const int rank, const int nRanks) {
     
-    const int stepsPerProcess = double(secuence.length()) / double(nRanks);
-    const int iStart = int( stepsPerProcess * rank );
-    const int iEnd = int( stepsPerProcess * (rank + 1));
+    const int stepsPerProcess = double(secuence.length()-1) / double(nRanks);
+    const int iStart = int(stepsPerProcess * rank);
+    const int iEnd = int(stepsPerProcess * (rank + 1));
     
     #pragma omp parallel for
     for (int i = iStart; i < iEnd; i++) {
